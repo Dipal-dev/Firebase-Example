7313190727f745106406e33e622c1d47bc4b77f1
dipalpatel913@gmail.com <dipalpatel913@gmail.com>
2019-11-21 13:08:59 -0500

    Initial Commit

---
+++ Firebase_authentication_CRUD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase_authentication_CRUD
+//
+//  Created by Dipal Patel on 2019-11-21.
+//  Copyright © 2019 Dipal Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
